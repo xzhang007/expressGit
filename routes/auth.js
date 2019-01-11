f9c4856c78449bd25a78b5f94751300ac27a1475
judy <judy@zenabi.com>
2019-01-11 14:10:02 -0500

    access the user information

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var github = require('octonode');
 
 router.post('/', function(request, response) {
-	//console.log("yes")
+	
 	//console.log(request.body.username)
 	//console.log(request.body.password)
 	var client = github.client({
@@ -12,9 +12,18 @@ router.post('/', function(request, response) {
 	});
 
 	client.get('/user', {}, function(err, status, body, headers) {
-		console.log(body);
+		//console.log(body);
+		response.render('user', {body: body
+				//username: body.login},
+				//{userid: body.id
+				//company: body.compay,
+				//location: body.location,
+				//followers: body.followers,
+				//following: body.following,
+				//email: email
+			});
 	})
-	response.render('auth', {username: request.body.username, password: request.body.password});
+	//response.render('auth', {username: request.body.username, password: request.body.password});
 	
 })
 
