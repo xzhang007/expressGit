db429e032ea5c7f23dd87bd664a5bed77acf7a13
judy <judy@zenabi.com>
2019-01-11 15:53:50 -0500

    modified /repo routes and view

--- routes/repo.js
+++ routes/repo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('repo');
+  res.render('repo', {data: req.body});
 });
 
 module.exports = router;
