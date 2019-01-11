1413cf3186701bf3a313a9ffd0b5c02ee375e185
judy <judy@zenabi.com>
2019-01-11 12:25:35 -0500

    install octonode to build a client from github credentials

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var github = require('octonode');
 
 router.post('/', function(request, response) {
 	//console.log("yes")
-	console.log(request.body.username)
-	console.log(request.body.password)
+	//console.log(request.body.username)
+	//console.log(request.body.password)
+	var client = github.client({
+		username: request.body.username,
+		password: request.body.password
+	});
+
+	client.get('/user', {}, function(err, status, body, headers) {
+		console.log(body);
+	})
 	response.render('auth', {username: request.body.username, password: request.body.password});
+	
 })
 
 
